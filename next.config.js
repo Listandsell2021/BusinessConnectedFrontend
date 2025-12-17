847007d956ab3880af9818c97939e3ecad2428cb
Muskan <muskanlistndsell@gmail.com>
2025-12-17 20:09:34 +0530

    versionm update

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: false
-  },
   i18n: {
     locales: ['en', 'de'],
     defaultLocale: 'de',
