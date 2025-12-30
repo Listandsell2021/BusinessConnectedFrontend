3d780f8183a2e94eac732eff6ebb8a1874bc9ab0
Muskan <muskanlistndsell@gmail.com>
2025-12-30 14:42:19 +0530

    dleted server's files

--- middleware.js
+++
@@ -1,7 +0,0 @@
-// Middleware disabled - using pure Pages Router
-// Language is managed by LanguageContext (localStorage)
-// No middleware needed for URL routing
-
-export const config = {
-  matcher: []  // Empty matcher = middleware does nothing
-};
